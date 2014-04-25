267ccb1d14a1672e1ab39e627bee053f217aee03
Pommesfee <klook.christian@gmail.com>
2014-04-25 20:52:07 +0200

    Basic Iinitialization

---
+++ TestGame/src/core/Main.java
@@ -0,0 +1,11 @@
+package core;
+
+public class Main {
+
+	// This is the start of a new project that will be some kind of a basic game.
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+	
+}
