84d033f95754f64778fb7c397cce91edcb1fc4ba
Pommesfee <klook.christian@gmail.com>
2014-04-26 23:08:52 +0200

    Second Commit

--- TestGame/src/core/Main.java
+++ TestGame/src/core/Main.java
@@ -1,11 +1,46 @@
 package core;
 
-public class Main {
+//import java.awt.DisplayMode;
+//import java.awt.GraphicsDevice;
+//import java.awt.GraphicsEnvironment;
 
-	// This is the start of a new project that will be some kind of a basic game.
+import javax.swing.JFrame;
+
+import gameobjects.Player;
+import gui.Frame;
+
+public class Main {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		
+		Player player = new Player(400, 300, 50, 300, 800, 600);
+		
+		Frame frame = new Frame(player);
+		frame.setSize(800, 600);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//		frame.setUndecorated(true);
+		frame.setVisible(true);
+		frame.setResizable(false);
+		frame.setLocationRelativeTo(null);
+		
+//		DisplayMode displayMode = new DisplayMode(800, 600, 16, 75);
+//		GraphicsEnvironment environment = GraphicsEnvironment.getLocalGraphicsEnvironment();
+//		GraphicsDevice device = environment.getDefaultScreenDevice();
+//		
+//		device.setFullScreenWindow(frame);
+//		device.setDisplayMode(displayMode);
+		
+		long lastFrame = System.currentTimeMillis();
+		while (true) {
+			long thisFrame = System.currentTimeMillis();
+			double timeSinceLastFrame = (double) (thisFrame - lastFrame) / 1000d;
+			lastFrame = thisFrame;
+			
+			player.update(timeSinceLastFrame, frame.getUp(), frame.getDown(), frame.getLeft(), frame.getRight());
+			
+			frame.repaintScreen();
+			
+		}
 	}
 	
 }
